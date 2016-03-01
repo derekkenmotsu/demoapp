fa8dc9c17144a819fda43b29ecfe0e1886aa8ba6
derekkenmotsu <derek_kenmotsu@mba.berkeley.edu>
2016-03-01 03:22:22 +0000

    added home help tests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,18 @@
+require 'test_helper'
+
 class StaticPagesController < ApplicationController
-  def home
+  test "should get home" do
+  get :home
+  assert_response :success
+  asset_select "hi", "DemoApp"
   end
 
+  test "should get help" do
+    get :help
+    assert_response :success
+    asset_select "hi", "help"
   def help
   end
+
 end
+
