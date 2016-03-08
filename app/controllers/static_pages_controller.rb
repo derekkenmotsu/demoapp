5eb958a84b26816161d078d9708ef9330d2e02a6
derekkenmotsu <derek_kenmotsu@mba.berkeley.edu>
2016-03-08 02:56:45 +0000

    “Bootstrap”

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,8 @@
-require 'test_helper'
-
 class StaticPagesController < ApplicationController
-  test "should get home" do
-  get :home
-  assert_response :success
-  asset_select "hi", "DemoApp"
+  def home
+    
   end
 
-  test "should get help" do
-    get :help
-    assert_response :success
-    asset_select "hi", "help"
   def help
   end
 
